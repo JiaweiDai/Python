21f71ec6900b5f6210263d7fe4b85d23fb1d6f9f
Jiawei Dai <daijiawei24@gmail.com>
2020-12-24 17:32:02 +0800

    It's a suck working day

---
+++ HelloPython/PythonIO.py
@@ -0,0 +1,11 @@
+# There are two main kind output : expressions and print()
+# You also could use write() output into a file, standard output sys.stdout.
+# str.format() to format output
+# repr() or str() turn output to string.
+# str() return an easy reading expression.
+# repr() produce an expression for interpreter easy read, paremeter could be any object.
+
+s = "Hello Python!"
+print(s)
+print(str(s))
+print(repr(s))
